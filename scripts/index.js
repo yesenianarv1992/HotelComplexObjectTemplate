8a6ba0f8b26fe4ea971d16c3c291ec42dd782dda
Cliff <tools@3csonline.com>
2017-11-20 04:54:43 +0000

    template complete

---
+++ scripts/index.js
@@ -0,0 +1,21 @@
+var hotel = {
+    rooms: [
+        {
+            name: "Double Bed",
+            price: "$75.00",
+            available: 40
+        },
+        {
+            name: "King Bed",
+            price: "$120.00",
+            available: 20
+        },
+        {
+            name: "Luxery Suite",
+            price: "$250.00",
+            available: 5
+        }
+    ],
+    name: "CareerDevs Innt"
+}
+
