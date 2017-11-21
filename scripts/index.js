2cdb503c2fed80e17b6e5b12565b8d61a220aeac
Yesenia Narvaez <yesenian1992@gmail.com>
2017-11-21 01:06:52 +0000

    classwork

--- scripts/index.js
+++ scripts/index.js
@@ -19,3 +19,20 @@ var hotel = {
     name: "CareerDevs Innt"
 }
 
+// console.log(hotel.rooms[1].price)
+
+//console.log(document.getElementById("radio").nodeName);
+//.log(document.getElementById("label").nodeName);
+
+for (var i = 0; i < hotel.rooms.length; i++){
+  var radioBtn = document.createElement("INPUT");
+  radioBtn.setAttribute("type", "radio");
+  radioBtn.setAttribute("name", "roomtype"); 
+  radioBtn.setAttribute("value", i);
+  radioBtn.setAttribute("id", "room" + i);
+  var radioLbl = document.createElement("LABEL");
+  radioLbl.innerHTML = hotel.rooms[i].name;
+    document.getElementById("radialSection").appendChild(radioBtn);
+    document.getElementById("radialSection").appendChild(radioLbl);
+    
+}
